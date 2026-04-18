8e958eea8e0ef19d663cf9d73c558dbfa6572314
DmitryM <mikhaltchenkov@gmail.com>
2026-04-18 17:42:22 +0500

    fixed dss exec fn

--- lib/src/dss/dss_exec.c
+++ lib/src/dss/dss_exec.c
@@ -3,6 +3,7 @@
 i16 dss_exec(const char *path) __naked {
     __asm
         push    ix
+        ld      b, #0           ; subfunction: EXEC0 (auto path handling)
         ld      c, #0x40
         rst     #0x10
         pop     ix
